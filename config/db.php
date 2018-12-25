8eedfb483057a27c3627e51527cede7eed982c69
Ziyat <termit_90@mail.ru>
2018-12-25 13:37:15 +0500

    fixed

--- config/db.php
+++ config/db.php
@@ -5,9 +5,13 @@ return [
 //    'dsn' => 'mysql:host=127.0.0.1;dbname=ilmondo',
 //    'username' => 'root',
 //    'password' => '123',
-    'dsn' => 'mysql:host=localhost;dbname=geco.uz',
-    'username' => 'geco',
-    'password' => 'PvPM1G52e9Qrs39F',
+    'dsn' => 'mysql:host=localhost;dbname=geco',
+    'username' => 'root',
+    'password' => 'qZD+AgJ]0#=Z1H_Br+3ja^%,6UZ%0wqbb$;3r%-^RPHhKhDPKo',
+
+//    'dsn' => 'mysql:host=localhost;dbname=geco.uz',
+//    'username' => 'geco',
+//    'password' => 'PvPM1G52e9Qrs39F',
     'charset' => 'utf8',
 
     // Schema cache options (for production environment)
