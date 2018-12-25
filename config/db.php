30a7b62b52a0dca3f0d81d2dacbc51addd693885
Ziyat <termit_90@mail.ru>
2018-12-25 13:04:34 +0500

    fixed

--- config/db.php
+++ config/db.php
@@ -2,12 +2,12 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=127.0.0.1;dbname=ilmondo',
-    'username' => 'root',
-    'password' => '123',
-//    'dsn' => 'mysql:host=localhost;dbname=geco',
+//    'dsn' => 'mysql:host=127.0.0.1;dbname=ilmondo',
 //    'username' => 'root',
-//    'password' => 'qZD+AgJ]0#=Z1H_Br+3ja^%,6UZ%0wqbb$;3r%-^RPHhKhDPKo',
+//    'password' => '123',
+    'dsn' => 'mysql:host=localhost;dbname=geco.uz',
+    'username' => 'geco',
+    'password' => 'PvPM1G52e9Qrs39F',
     'charset' => 'utf8',
 
     // Schema cache options (for production environment)
