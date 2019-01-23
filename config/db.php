0947aae601d26dd7de0e5bb95d5a1a0b837e8d3e
Ziyat <termit_90@mail.ru>
2019-01-23 20:00:47 +0500

    fixed

--- config/db.php
+++ config/db.php
@@ -3,7 +3,7 @@
 return [
     'class' => 'yii\db\Connection',
 //    'dsn' => 'mysql:host=127.0.0.1;dbname=new_geco',
-    'dsn' => 'mysql:host=127.0.0.1;dbname=geco.uz',
+    'dsn' => 'mysql:host=localhost;dbname=geco.uz',
     'username' => 'geco',
     'password' => 'KDws8hyxHmWKBaxf',
     'charset' => 'utf8mb4',
