cc8a34984518b8060a928e2a0e53d4e1cc7743ea
Ziyat <termit_90@mail.ru>
2019-01-24 23:37:55 +0500

    fixed

--- config/db.php
+++ config/db.php
@@ -1,5 +1,5 @@
 <?php
-if ($_SERVER['SERVER_NAME'] == 'http://new.geco.test' || $_SERVER['SERVER_NAME'] == 'localhost') {
+if ($_SERVER['SERVER_NAME'] == 'http://new.geco.test') {
     $db = [
         'dsn' => 'mysql:host=localhost;dbname=geco.uz',
         'username' => 'geco',
